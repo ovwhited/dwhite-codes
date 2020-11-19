b2d53f980f800f2aa4764cf56421eb9ee09aaf03
Derek White <ov.whited@gmail.com>
2020-11-18 23:31:14 -0500

    Removes EJS rendering

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const app = express();
-app.set('view engine', 'ejs');
-app.use(express.static('static'));
+
+app.use(express.static('static', { index: 'index.html' }));
 
 const nedb = require('nedb-promises');
 const db = new nedb({ filename: 'posts.ndjson', autoload: true });
 const marked = require('marked');
 
-app.get('/', async (req, res) => {
-  const posts = (await db.find({}))
-    .map(post => (post.body_markdown = marked(post.body_markdown)) && post);
+app.get('/posts.json', async (req, res) => {
+  let posts;
+  let limit = req.query.limit ? req.query.limit : 500;
+  let target_fields = req.query.fields ?
+    req.query.fields.split(',').reduce((o, field) => (o[field] = 1) && o, {}) : {};
 
-  res.render('index', {posts: posts});
+  try {
+    posts = (await db.find({}, target_fields).sort({ created_ts: -1 }).limit(limit))
+      .map(post => {
+        if(post.body_markdown) {
+          post.body_markdown = marked(post.body_markdown);
+        }
+        return post;
+      });
+
+    res.json(posts);
+  }
+  catch(error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 });
 
-app.listen(80);
+app.listen(8001);
